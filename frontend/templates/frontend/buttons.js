53c3933f034b0ae9e94afc044e51c2734d3c2f43
Annabel-13 <AnnabelFeer@gmail.com>
2024-02-17 13:44:29 +0100

    first screen

---
+++ frontend/templates/frontend/buttons.js
@@ -0,0 +1,8 @@
+// terms of use
+document.getElementById("termsOfUseButton").addEventListener("click", function() {
+    let content = document.getElementById("termsOfUseContent");
+            content.style.display = content.style.display === "none" ? "block" : "none";
+            // let uri = 'https://10.12.14.3/legal';
+            // window.location.href = uri;
+       });
+//end of terms of use
